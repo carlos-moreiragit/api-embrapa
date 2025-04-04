b46db1e16daa1b907ad7756d3c07714ba09854a1
Carlos Moreira <crmoreira@gmail.com>
2025-04-04 09:23:29 -0300

    Separando models

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from models import User, db, Production
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 swagger = Swagger(app)
 jwt = JWTManager(app)
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+db.init_app(app)
 
 @app.route("/register", methods=["POST"])
 def register_user():
@@ -47,3 +43,65 @@ def register_user():
     db.session.commit()
     
     return jsonify({"message": "User registered successfully"}), 201
+
+@app.route("/login", methods=["POST"])
+def login():
+    """
+    Faz login do usuário e retorna um JWT.
+    ---
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+    responses:
+      200:
+        description: Login bem sucedido, retorna JWT
+      401:
+        description: Credenciais inválidas
+    """
+    data = request.get_json()
+    user = User.query.filter_by(username=data["username"]).first()
+    if user and user.password == data["password"]:
+        # converter o ID para string
+        token = create_access_token(identity=str(user.id))
+        return jsonify({"access_token": token}), 200
+    return jsonify({"error": "Invalid credentials"}), 401
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    """
+    Rota protegida por autenticação
+    ---
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+            type: object
+            properties:
+                username:
+                    type: string
+                password:
+                    type: string
+
+    responses:
+      200:
+        description: Rota protegida acessada com sucesso
+      400:
+        description: Erro de autenticação
+    """
+    current_user_id = get_jwt_identity()
+    return jsonify({"msg": f"Usuário com ID {current_user_id} acessou a rota protegida"}), 200
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
